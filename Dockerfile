7d688839447cede16f59574041422d6736665b53
klim <klim@klim.by>
2018-08-26 14:47:25 +0300

    0.1.1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM postgres:10.5
-#FROM postgres:10.5-alpine
 LABEL e-postgres.description="postgres:10.5"
-LABEL e-postgres.version="0.1.0"
+LABEL e-postgres.version="0.1.2"
 
 RUN localedef -i ru_RU -c -f UTF-8 -A /usr/share/locale/locale.alias ru_RU.UTF-8
 ENV LANG ru_RU.utf8
--- build.sh
+++ build.sh
@@ -1,16 +1,35 @@
 #!/bin/bash -l
 #$ -S /bin/bash
-echo $1
-if [[ $1 ]]
-then
-    docker build -t klimby/e-postgres:$1 -t klimby/e-postgres:latest .
-    docker push klimby/e-postgres:$1
+#echo $1
+#if [[ $1 ]]
+#then
+#    docker build -t klimby/e-postgres:$1 -t klimby/e-postgres:latest .
+#    docker push klimby/e-postgres:$1
+#    docker push klimby/e-postgres:latest
+#    rm *.tar
+#    docker save klimby/e-postgres:$1 klimby/e-postgres:latest > e-postgres.$1.tar
+#    ls -sh e-postgres.$1.tar
+#    git tag -a $1 -m "version $1"
+#    git commit -m "Version $1"
+#    git push origin master
+#else
+#     echo -e "\033[31m Отсутствует номер версии \033[0m"
+#fi
+
+
+PACKAGE_VERSION=$(cat Dockerfile \
+  | grep LABEL \
+  | grep version \
+  | awk -F= '{print $2}' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]'
+ )
+
+docker build -t klimby/e-postgres:$PACKAGE_VERSION -t klimby/e-postgres:latest .
+    docker push klimby/e-postgres:$PACKAGE_VERSION
     docker push klimby/e-postgres:latest
-    docker save klimby/e-postgres:$1 klimby/e-postgres:latest > e-postgres.$1.tar
-    ls -sh e-postgres.$1.tar
-else
-     echo -e "\033[31m Отсутствует номер версии \033[0m"
-fi
-
-
-
+    rm *.tar
+    docker save klimby/e-postgres:$PACKAGE_VERSION klimby/e-postgres:latest > e-postgres.$PACKAGE_VERSION.tar
+    ls -sh e-postgres.$PACKAGE_VERSION.tar
+    git tag -a $PACKAGE_VERSION -m "version $PACKAGE_VERSION"
+    git commit -m "Version $PACKAGE_VERSION docker complete"
